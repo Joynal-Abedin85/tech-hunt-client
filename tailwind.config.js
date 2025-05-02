6abec613584876321d296f1f2d7bb593a841d7b8
kazi tareq <kazitareq321@gmail.com>
2025-05-02 20:32:04 +0600

    add color on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,14 @@ export default {
       fontFamily: {
         calligraphy: ['Great Vibes', 'cursive'], // Add Great Vibes as the calligraphy font
       },
+      colors: {
+        bg: '#0F172A',
+        text: '#F8FAFC',
+        hover: '#38BDF8',
+        border: '#334155',
+        card: '#1E293B',
+        primary: '#6366F1',
+      },
     },
   },
   plugins: [
